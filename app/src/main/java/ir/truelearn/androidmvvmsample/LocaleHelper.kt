5151fd9dd9ec0cbdf50ababb2b14349f7ee159c5
peyvandjadidi <peyvandjadidi@gmail.com>
2023-01-07 22:55:40 +0330

    multiple-language logic has been set #4 closes.

---
+++ app/src/main/java/ir/truelearn/androidmvvmsample/LocaleHelper.kt
@@ -0,0 +1,42 @@
+package ir.truelearn.androidmvvmsample
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import android.view.View
+import java.util.Locale
+
+object LocaleHelper {
+
+    fun setLocale(context: Context,language: String):Context?{
+        App.myLanguage = language
+         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            updateResource(context,language)
+        } else {
+            updateResourcesLegacy(context,language)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun updateResource(context: Context,language : String) : Context? {
+        val locale : Locale = Locale(language)
+        Locale.setDefault(locale)
+        val configuration : Configuration = context.resources.configuration
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+        return context.createConfigurationContext(configuration)
+    }
+
+    private fun updateResourcesLegacy(context: Context,language: String) : Context? {
+        val locale  = Locale(language)
+        Locale.setDefault(locale)
+        val resources = context.resources
+        val configuration : Configuration = resources.configuration
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+        resources.updateConfiguration(configuration,resources.displayMetrics)
+        return context
+    }
+}
