cee2b56f94fce4b3c750693c034d9676e66f5e6b
armin <the.yoldash@gmail.com>
2023-01-08 16:50:33 +0330

    multiple-language logic has been set #4 closes.

---
+++ app/src/main/java/ir/truelearn/androidmvvmsample/ui/component/ShowMoreProductsCard.kt
@@ -0,0 +1,60 @@
+package ir.truelearn.androidmvvmsample.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ir.truelearn.androidmvvmsample.ui.theme.DigikalaLightRed
+import ir.truelearn.androidmvvmsample.ui.theme.darkText
+import javax.sql.RowSet
+
+@Composable
+fun ShowMoreItem() {
+    Card(
+        modifier = Modifier
+            .size(170.dp, 358.dp)
+            .padding(vertical = 16.dp, horizontal = 4.dp),
+        shape = RoundedCornerShape(6.dp),
+        elevation = 4.dp,
+        contentColor = Color.White
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(top = 24.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = ir.truelearn.androidmvvmsample.R.drawable.show_more),
+                contentDescription = "",
+                tint = MaterialTheme.colors.DigikalaLightRed,
+                modifier = Modifier.size(40.dp,40.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                text = "مشاهده همه",
+                style = MaterialTheme.typography.body2,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colors.darkText,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                fontSize = 13.sp
+            )
+        }
+    }
+
+}
--- app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/HomeScreen.kt
+++ app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/HomeScreen.kt
@@ -34,6 +34,7 @@ import ir.truelearn.androidmvvmsample.navigation.Screen
 import ir.truelearn.androidmvvmsample.ui.component.AmazingItem
 import ir.truelearn.androidmvvmsample.ui.component.ProposalCards
 import ir.truelearn.androidmvvmsample.ui.component.SearchBar
+import ir.truelearn.androidmvvmsample.ui.component.ShowMoreItem
 import ir.truelearn.androidmvvmsample.ui.theme.DigikalaLightRed
 import ir.truelearn.androidmvvmsample.ui.theme.searchBarBg
 import ir.truelearn.androidmvvmsample.ui.theme.unSelectedBottomBar
@@ -69,15 +70,7 @@ fun Home() {
 
                     Amazing()
 
-                    // url proposal card list...
-                    val urlList = listOf<String>(
-                        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4f58b192407ed83e4271555cf8f4213409229189_1672864993.jpg?x-oss-process=image/quality,q_95",
-                        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4355945a3ff60f3dbe6a6d31fdb86bba0281aaa6_1668268672.jpg?x-oss-process=image/quality,q_95",
-                        "https://dkstatics-public.digikala.com/digikala-adservice-banners/e2023fd29340b5a4e0569a13651d2a9af34ff87d_1672834922.jpg?x-oss-process=image/quality,q_95",
-                        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4244d085416507f88a92e433ded27e05690e5f6b_1672865205.jpg?x-oss-process=image/quality,q_95"
-                    )
-                    ProposalCards(urlList)
-
+                    ProposalCards()
                 }
             }
 
@@ -96,7 +89,16 @@ fun Home() {
     }
 
 }
-
+@Composable
+fun ProposalCards(){
+    val urlList = listOf(
+        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4f58b192407ed83e4271555cf8f4213409229189_1672864993.jpg?x-oss-process=image/quality,q_95",
+        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4355945a3ff60f3dbe6a6d31fdb86bba0281aaa6_1668268672.jpg?x-oss-process=image/quality,q_95",
+        "https://dkstatics-public.digikala.com/digikala-adservice-banners/e2023fd29340b5a4e0569a13651d2a9af34ff87d_1672834922.jpg?x-oss-process=image/quality,q_95",
+        "https://dkstatics-public.digikala.com/digikala-adservice-banners/4244d085416507f88a92e433ded27e05690e5f6b_1672865205.jpg?x-oss-process=image/quality,q_95"
+    )
+    ProposalCards(urlList)
+}
 @Composable
 private fun Amazing() {
     Column(modifier = Modifier.fillMaxSize()) {
@@ -104,6 +106,9 @@ private fun Amazing() {
         LazyRow(modifier = Modifier.background(MaterialTheme.colors.DigikalaLightRed)) {
             items(20) {
                 AmazingItem()
+            }
+            item {
+                ShowMoreItem()
             }
         }
 
