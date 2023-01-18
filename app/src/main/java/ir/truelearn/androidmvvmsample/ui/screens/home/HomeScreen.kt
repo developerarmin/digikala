331eda9eafcca874a1d040e8823958afcf395a2a
reza azimvand <ontheroid@yahoo.com>
2023-01-18 16:51:48 +0330

    changes again after the update project

--- app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/home/HomeScreen.kt
@@ -1,46 +1,31 @@
 package ir.truelearn.androidmvvmsample.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.util.Log
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import ir.truelearn.androidmvvmsample.R
 import ir.truelearn.androidmvvmsample.data.model.AmazingItem
 import ir.truelearn.androidmvvmsample.data.remote.NetworkResult
-import ir.truelearn.androidmvvmsample.navigation.Screen
 import ir.truelearn.androidmvvmsample.ui.component.*
+import ir.truelearn.androidmvvmsample.ui.screens.home.AmazingItem
+import ir.truelearn.androidmvvmsample.ui.screens.home.SearchBar
+import ir.truelearn.androidmvvmsample.ui.screens.home.ShowMoreItem
 import ir.truelearn.androidmvvmsample.ui.theme.DigikalaLightRed
-import ir.truelearn.androidmvvmsample.ui.theme.searchBarBg
-import ir.truelearn.androidmvvmsample.ui.theme.unSelectedBottomBar
 import ir.truelearn.androidmvvmsample.viewmodel.HomeViewModel
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collectLatest
@@ -52,7 +37,7 @@ fun HomeScreen(navController: NavHostController) {
 
 
 @Composable
-fun Home() {
+fun Home(viewModel: HomeViewModel = hiltViewModel()) {
     if (!isSystemInDarkTheme()) {
         Column(
             modifier = Modifier
@@ -70,8 +55,11 @@ fun Home() {
                         .fillMaxSize()
                         .verticalScroll(rememberScrollState())
                 ) {
-
-                    Amazing()
+                    // connect to server and get all data
+                    LaunchedEffect(key1 = true) {
+                        viewModel.getAllDataFromServer()
+                    }
+                    InitAmazingItems()
 
                     ProposalCards()
                 }
@@ -159,11 +147,11 @@ fun ProposalCards() {
         "https://dkstatics-public.digikala.com/digikala-adservice-banners/e2023fd29340b5a4e0569a13651d2a9af34ff87d_1672834922.jpg?x-oss-process=image/quality,q_95",
         "https://dkstatics-public.digikala.com/digikala-adservice-banners/4244d085416507f88a92e433ded27e05690e5f6b_1672865205.jpg?x-oss-process=image/quality,q_95"
     )
-    ProposalCards(urlList)
+    ir.truelearn.androidmvvmsample.ui.screens.home.ProposalCards(urlProposalList = urlList)
 }
 
 @Composable
-private fun Amazing(
+private fun InitAmazingItems(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     var list by remember {
@@ -173,7 +161,7 @@ private fun Amazing(
         mutableStateOf(false)
     }
     LaunchedEffect(key1 = true) {
-        viewModel.getAmazingItems()
+
         viewModel.amazingItems.collectLatest {
             when (it) {
                 is NetworkResult.Success -> {
@@ -183,6 +171,8 @@ private fun Amazing(
                     }
                 }
                 is NetworkResult.Error -> {
+                    loading = false
+                    Log.d("2121", "InitAmazingItems error:${it.message} ")
                     // show error message
                 }
                 is NetworkResult.Loading -> {
