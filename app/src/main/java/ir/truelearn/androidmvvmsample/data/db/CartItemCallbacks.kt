44f3564373e87e3c88d0431406233fff6acdb6d2
reza azimvand <ontheroid@yahoo.com>
2023-02-01 06:12:08 +0330

    added cart item callbacks

---
+++ app/src/main/java/ir/truelearn/androidmvvmsample/data/db/CartItemCallbacks.kt
@@ -0,0 +1,11 @@
+package ir.truelearn.androidmvvmsample.data.db
+
+import ir.truelearn.androidmvvmsample.data.model.basket.CartItem
+import ir.truelearn.androidmvvmsample.data.model.basket.CartStatus
+
+interface CartItemCallbacks {
+    fun onRemoveCartItem(cart: CartItem)
+    fun onIncreaseCartItem(itemID: Int, newCount: Int)
+    fun onDecreaseCartItem(itemID: Int, newCount: Int)
+    fun onChangeStatusCart(itemID: Int, newStatus: CartStatus)
+}
