ee740ae9240bb8f0abfc7132313f6d5d715d32cd
Hossein <hossein.arabameri69@gmail.com>
2023-02-05 21:03:43 +0330

    change design DeliveryMethodSection

--- app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/checkout/DeliveryMethodSection.kt
+++ app/src/main/java/ir/truelearn/androidmvvmsample/ui/screens/checkout/DeliveryMethodSection.kt
@@ -1,6 +1,7 @@
 package ir.truelearn.androidmvvmsample.ui.screens.checkout
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -19,8 +20,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,6 +31,7 @@ import ir.truelearn.androidmvvmsample.R
 import ir.truelearn.androidmvvmsample.data.model.basket.CartItem
 import ir.truelearn.androidmvvmsample.data.model.basket.CartStatus
 import ir.truelearn.androidmvvmsample.ui.theme.darkText
+import ir.truelearn.androidmvvmsample.ui.theme.font_medium
 import ir.truelearn.androidmvvmsample.ui.theme.font_standard
 import ir.truelearn.androidmvvmsample.ui.theme.spacing
 import ir.truelearn.androidmvvmsample.util.DigitHelper
@@ -50,41 +54,65 @@ fun DeliveryMethodSection(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 4.dp),
+            .padding(
+                top = MaterialTheme.spacing.small,
+                bottom = MaterialTheme.spacing.small,
+                start = MaterialTheme.spacing.small,
+                end = MaterialTheme.spacing.small
+            ),
         horizontalAlignment = End
     ) {
         Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = "شیوه و زمان ارسال",
+            modifier = Modifier.padding(horizontal = MaterialTheme.spacing.mediumTwo),
+            text = String.format(stringResource(id = R.string.deliveryAndTimeMethod)),
             fontWeight = FontWeight.Bold,
+            fontFamily = font_medium
         )
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp, horizontal = 8.dp)
+                .padding(
+                    vertical = MaterialTheme.spacing.medium,
+                    horizontal = MaterialTheme.spacing.small
+                )
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 16.dp, horizontal = 8.dp),
+                    .padding(
+                        vertical = MaterialTheme.spacing.medium,
+                        horizontal = MaterialTheme.spacing.medium
+                    ),
                 horizontalAlignment = End
             ) {
                 Text(
-                    text = "مرسوله 1 از 1",
+                    text = String.format(stringResource(id = R.string.consignment)),
                     fontWeight = FontWeight.Bold,
                     style = MaterialTheme.typography.body1,
                     color = MaterialTheme.colors.darkText,
+                    fontFamily = font_medium
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Spacer(modifier = Modifier.weight(1f))
-                    Text(text = "4 کالا")
-//                    Icon(painter = painterResource(id = ), contentDescription = "")
-                    Text(text = "ارسال عادی")
-//                    Icon(painter = painterResource(id = ), contentDescription = "")
+                    Text(text = String.format(stringResource(id = R.string.commodity2))
+                        ,Modifier.padding(MaterialTheme.spacing.small),
+                    fontFamily = font_medium)
+                    Icon(modifier = Modifier
+                        .size(8.dp),
+                        painter = painterResource(id = R.drawable.ic_baseline_circle_24),
+                        contentDescription = "")
+                    Text(text = String.format(stringResource(id = R.string.normalDelivery)),
+                        Modifier.padding(MaterialTheme.spacing.small),
+                    fontFamily = font_medium,)
+                    Icon(modifier = Modifier
+                        .size(20.dp),
+                        painter = painterResource(id = R.drawable.delivery_truck),
+                        contentDescription = "")
                 }
                 LazyRow {
                     items(5){
@@ -94,31 +122,19 @@ fun DeliveryMethodSection(
 //                        CheckoutProductCard(item = item)
 //                    }
                 }
-                Text(text = "آماده ارسال")
+                Text(text = String.format(stringResource(id = R.string.readyToSend)),
+                fontFamily = font_medium)
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 5.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                        .padding(vertical = MaterialTheme.spacing.small),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        modifier = Modifier,
-                        text = "هزینه ارسال",
-                        textAlign = TextAlign.Start,
-                        style = MaterialTheme.typography.h6,
-                        fontWeight = FontWeight.Medium,
-                        color = Color.Gray,
-                        fontFamily = font_standard,
-                    )
-                    Row() {
-                        Text(
-                            text = DigitHelper.digitByLocate(shippingConst),
-                            style = MaterialTheme.typography.body2,
-                            fontWeight = FontWeight.Medium,
-                            color = Color.Black
-                        )
 
+                    Row(modifier = Modifier
+                        .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End) {
                         Image(
                             painter = painterResource(id = R.drawable.toman),
                             contentDescription = "",
@@ -126,7 +142,20 @@ fun DeliveryMethodSection(
                                 .size(MaterialTheme.spacing.semiLarge)
                                 .padding(horizontal = MaterialTheme.spacing.extraSmall)
                         )
-
+                        Text(
+                            text = DigitHelper.digitByLocate(shippingConst),
+                            style = MaterialTheme.typography.body2,
+                            fontWeight = FontWeight.Medium,
+                            color = Color.Black
+                        )
+                        Text(
+                            modifier = Modifier,
+                            text = String.format(stringResource(id = R.string.costSend)),
+                            style = MaterialTheme.typography.h6,
+                            fontWeight = FontWeight.Medium,
+                            color = Color.Gray,
+                            fontFamily = font_standard
+                        )
                     }
                 }
                 Row(
@@ -140,7 +169,7 @@ fun DeliveryMethodSection(
 
 
                     Text(
-                        text = "انتخاب زمان ارسال",
+                        text = String.format(stringResource(id = R.string.choseTimeSend)),
                         fontWeight = FontWeight.SemiBold,
                         style = MaterialTheme.typography.h6,
                         color = colorResource(id = R.color.iconColor)
